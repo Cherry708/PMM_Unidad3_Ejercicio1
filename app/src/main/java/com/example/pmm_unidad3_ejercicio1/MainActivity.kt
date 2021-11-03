6ecfa023389faf75c2b21d744bf9c21c65d13975
Cherry708 <aluser8241@ieselcaminas.org>
2021-11-03 18:42:50 +0100

    No TextWatcher

--- app/src/main/java/com/example/pmm_unidad3_ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/pmm_unidad3_ejercicio1/MainActivity.kt
@@ -1,35 +1,47 @@
 package com.example.pmm_unidad3_ejercicio1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val spnOperacion = findViewById<Spinner>(R.id.spnOperacion)
+
         val adaptador = ArrayAdapter.createFromResource(
-            this, R.array.listaOperaciones, android.R.layout.simple_spinner_item)
+            this, R.array.listaOperaciones, android.R.layout.simple_spinner_item
+        )
+
         adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
         spnOperacion.adapter = adaptador
-        spnOperacion.onItemSelectedListener
-    }
 
-    override fun onItemSelected(item: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-        //An item was selected. You can retrieve the selected item using
-        //parent.getItemAtPosition(pos)
-        if (item?.selectedItemPosition == 0){
-            Toast.makeText(this,"Prs", Toast.LENGTH_SHORT).show()
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        val etNumber0 = findViewById<EditText>(R.id.etNumero0)
+        val etNumber1 = findViewById<EditText>(R.id.etNumero1)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+
+        btnCalcular.setOnClickListener {
+
+            val number0 = etNumber0.text.toString().toInt()
+            val number1 = etNumber1.text.toString().toInt()
+            when (spnOperacion.selectedItemPosition) {
+                0 -> tvResultado.setText("${number0 + number1}")
+                1 -> tvResultado.setText("${number0 - number1}")
+                2 -> tvResultado.setText("${number0 * number1}")
+                3 -> tvResultado.setText("${number0 / number1}")
+            }
         }
-    }
-
-    override fun onNothingSelected(p0: AdapterView<*>?) {
-        TODO("Not yet implemented")
     }
 }
